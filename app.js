7470697216f8705d301bdb48a55aef14e4a01505
AndreyKapus <kapustnikov@ukr.net>
2023-05-11 16:07:28 +0300

    date

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
+const fs = require('fs/promises');
+const moment = require('moment');
 
-const contacts = require('./contacts')
+const contacts = require('./contacts');
+
+app.use( async (req, res, next) => {
+    const {method, url} = req;
+    const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+   await fs.appendFile('./public/server.log', `${method} ${url} ${date}`);
+   next()
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/contacts', (req, res) => {
+    res.json(contacts);
+})
+
 app.listen(3001, () => {
   console.log('Example app listening on port 3001!');
 });
 
-app.get('/contacts', (req, res) => {
-    res.json(contacts);
-})
