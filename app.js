b4720ef7ac2666949c28897c17d59c95ec8afb15
AndreyKapus <kapustnikov@ukr.net>
2023-05-11 17:20:34 +0300

    cors

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const fs = require('fs/promises');
 const moment = require('moment');
+const cors = require('cors')
 
 const contacts = require('./contacts');
+
+const corsMidleware = cors();
+
+app.use(corsMidleware)
 
 app.use( async (req, res, next) => {
     const {method, url} = req;
@@ -18,6 +23,12 @@ app.get('/', (req, res) => {
 
 app.get('/contacts', (req, res) => {
     res.json(contacts);
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Not found'
+  })
 })
 
 app.listen(3001, () => {
