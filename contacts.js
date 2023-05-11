a3f80e426fb2b073dbcd7a496c841533f2b43fe1
AndreyKapus <kapustnikov@ukr.net>
2023-05-11 15:16:29 +0300

    contacts

--- contacts.js
+++ contacts.js
@@ -1,7 +1,13 @@
 
-[  
+module.exports = [  
     {
         "name": "andrii",
         "number": "3214214214"
-    }
+    },
+
+    {
+        "name": "nil",
+        "number": "64514214214"
+    },
+    
 ]
