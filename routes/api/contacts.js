f1412be3bca32075f8d13c3179b709c2f16dc07f
AndreyKapus <kapustnikov@ukr.net>
2023-05-11 19:32:59 +0300

    nanoid

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 const contacts = require('../../models/contacts/contacts');
+const { getAll } = require('../../models/contacts');
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.json(contacts);
+router.get('/', async (req, res) => {
+    const responce = await getAll()
+    responce.json(contacts);
 });
 
 module.exports = router;
