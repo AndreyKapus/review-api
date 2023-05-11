31d04805059c89caabe5be97a6c82b88aa4fcf08
AndreyKapus <kapustnikov@ukr.net>
2023-05-11 15:11:27 +0300

    nodemon

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+const contacts = require('./contacts')
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Example app listening on port 3001!');
 });
+
+app.get('/contacts', (req, res) => {
+    res.json(contacts);
+})
