335ee26a9655796d57e9e2334efbe68c91be5765
AndreyKapus <kapustnikov@ukr.net>
2023-05-20 13:11:46 +0300

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
 const {User} = require('../models/user/user');
 const HttpError = require('../helpers/HttpError');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 const { ctrlWrapper } = require('../helpers');
 
+const {SECRET_KEY} = process.env
 
 const register = async (req, res) => {
     const {email, password} = req.body;
@@ -22,6 +24,32 @@ const register = async (req, res) => {
     })
 };
 
+const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+    if(!user) {
+        throw HttpError(401, "Email or password invalid")
+    };
+
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if(!comparePassword) {
+        throw HttpError(401, "Email or password invalid")
+}
+
+    const payload = {
+        id: user._id
+    };
+
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '23h'});
+
+    res.json({
+        token,
+    });
+};
+
+
 module.exports = {
     register: ctrlWrapper(register),
+    login: ctrlWrapper(login)
 };
