a89361a64e1bc21214cf2621d2ef810b6ae9d7ee
AndreyKapus <kapustnikov@ukr.net>
2023-06-11 13:18:10 +0300

    redirect

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,7 +46,7 @@ const verifyEmail = async (req, res) => {
 
     await User.findByIdAndUpdate(user._id, {verify: true, verificationCode: ''});
 
-    res.redirect(process.env.CLIENT_URL)
+    return res.redirect(process.env.CLIENT_URL)
 };
 
 const resendVerifyEmail = async (req, res) => {
