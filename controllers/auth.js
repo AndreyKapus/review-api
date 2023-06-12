0fbed11cc37d214cc52f3500c68e792dd3da0cc5
AndreyKapus <kapustnikov@ukr.net>
2023-06-12 11:03:58 +0300

    red

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,10 +46,7 @@ const verifyEmail = async (req, res) => {
 
     await User.findByIdAndUpdate(user._id, {verify: true, verificationCode: ''});
     
-    res.redirect('http://localhost:3000')
-    // res.json({
-    //     message: "Succsess",
-    // })
+    res.redirect('http://localhost:3000/login');
 };
 
 const resendVerifyEmail = async (req, res) => {
